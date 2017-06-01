2690829793d9ecb4a8a2a80a00eed70b90001844
V-in <vinicius_gbapb@icloud.com>
2017-06-01 20:59:29 -0300

    Striped out more unused code

--- Projeto_easy/src/Detector.cpp
+++ Projeto_easy/src/Detector.cpp
@@ -18,24 +18,11 @@ void Detector::detect(cv::Mat& img, double scale){
     double t = 0;
     assert(!img.empty());
     std::vector<cv::Rect> faces, faces2;
-    const static cv::Scalar colors[] =
-    {
-        cv::Scalar(255,0,0),
-        cv::Scalar(255,128,0),
-        cv::Scalar(255,255,0),
-        cv::Scalar(0,255,0),
-        cv::Scalar(0,128,255),
-        cv::Scalar(0,255,255),
-        cv::Scalar(0,0,255),
-        cv::Scalar(255,0,255)
-    };
     cv::Mat gray, smallImg;
     cv::cvtColor( img, gray, cv::COLOR_BGR2GRAY );
     double fx = 1 / scale;
     cv::resize( gray, smallImg, cv::Size(), fx, fx, cv::INTER_LINEAR );
     cv::equalizeHist( smallImg, smallImg );
-
-
     t = (double)cv::getTickCount();
     classifier.detectMultiScale( smallImg, faces,
         1.1, 2, 0
@@ -45,13 +32,12 @@ void Detector::detect(cv::Mat& img, double scale){
         cv::Size(30, 30) );
 
     t = (double)cv::getTickCount() - t;
-    printf( "detection time = %g ms\n", t*1000/cv::getTickFrequency());
-
+   // printf( "detection time = %g ms\n", t*1000/cv::getTickFrequency());
+    if(faces.size() == 0) imshow("result", img);
     for ( size_t i = 0; i < faces.size(); i++ ){
         cv::Rect r = faces[i];
         cv::Mat smallImgROI;
         std::vector<cv::Rect> nestedObjects;
-        cv::Scalar color = colors[i%8];
         int radius;
 
         double aspect_ratio = (double)r.width/r.height;
@@ -59,12 +45,12 @@ void Detector::detect(cv::Mat& img, double scale){
             center.x = cvRound((r.x + r.width*0.5)*scale);
             center.y = cvRound((r.y + r.height*0.5)*scale);
             radius = cvRound((r.width + r.height)*0.25*scale);
-            cv::circle( img, center, radius, color, 3, 8, 0 );      
+            cv::circle( img, center, radius, cv::Scalar(255,0,0), 3, 8, 0 );      
         }
-        else
-            rectangle( img, cvPoint(cvRound(r.x*scale), cvRound(r.y*scale)),
-                       cvPoint(cvRound((r.x + r.width-1)*scale), cvRound((r.y + r.height-1)*scale)),
-                       color, 3, 8, 0);
+        else{
+            center = cvPoint(cvRound((r.x + r.width-1)*scale), cvRound((r.y + r.height-1)*scale));
+        }
+        std::cout << center.x << " " << center.y << std::endl;
+        imshow("result", img);
     }
-    imshow( "result", img );
 }
